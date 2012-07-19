5cc50e87953c4c746b1b1e67768889db70c71410
FreeleX <fallforfreedom@gmail.com>
2012-07-19 17:24:36 +0300

    Added notifications using libnotify + small cleanup

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -2,3 +2,4 @@ pref("extensions.unity-integration.firstrun", true);
 
 pref("extensions.unity-integration.debugMode", false);
 pref("extensions.unity-integration.hideOnClose", true);
+pref("extensions.unity-integration.enableNotifications", true);
